62ae5dc302e82ad6b3cffb151bc647b6770aca33
Wenxi <zoezhang94@gmail.com>
2023-11-09 19:31:17 -0500

    exercise

--- basics/server2.js
+++ basics/server2.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.listen(port, () => {
-  console.log(`app listening on ${port}`);
-});
 
 app.use(express.json());
 
@@ -13,19 +10,21 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  return res.send({ping: 'pong'});
-});
-
 app.use(function (req, res, next) {
   console.log('hello');
   next(new NotFoundError());
 });
 
 app.use(function(err, req, res, next) {
-  const status = err.status || 500;
-  const message = err.message;
-  return res.status(status).json({
-    error: {message, status}
-  });
+  console.log(`Error occurred: ${err.message}`);
+  next(); 
+
+});
+
+app.get('/', (req, res) => {
+  return res.send({ping: 'pong'});
+});
+
+app.listen(port, () => {
+  console.log(`app listening on ${port}`);
 });
